87d5f3525ae95aac55da33af24bf48ad59087a5e
Rajasree Laha <laharajasree121@gmail.com>
2024-04-04 22:02:55 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  /* background: linear-gradient(hsl(254, 31%, 54%), #42006c); */
+  background-image: url("./components/Assets/photo.jpeg");
+  /* background-size: cover; */
+  /* background-repeat: no-repeat; */
+  /* background-position: center; */
+  backdrop-filter: blur(2.5px);
+}
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
